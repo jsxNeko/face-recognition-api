426159769c6b0990ba8d3a5d0423482996911bee
Serenuy <dserena01@yahoo.com>
2022-08-26 11:01:58 -0500

    Fixed variable name

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const db = require('knex')({
   client: 'pg',
   version: '7.2',
   connection: {
-    host : process.env.DATABASE_URL,
+    connectionString : process.env.DATABASE_URL,
     rejectUnauthorized: false
   }
 });
